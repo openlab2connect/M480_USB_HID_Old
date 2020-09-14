cffbf0b06ac57f2ec249f6c799552cd8f78dead4
KT <karl.tsou@hotmail.com>
2020-09-14 11:12:50 +0800

    Merge: USB HID sample

---
+++ Debug/User/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../User/descriptors.c \
+../User/hid_transfer.c \
+../User/main.c 
+
+OBJS += \
+./User/descriptors.o \
+./User/hid_transfer.o \
+./User/main.o 
+
+C_DEPS += \
+./User/descriptors.d \
+./User/hid_transfer.d \
+./User/main.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+User/%.o: ../User/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM GNU C Compiler'
+	arm-none-eabi-gcc -mcpu=cortex-m4 -mthumb -mfloat-abi=hard -mfpu=fpv4-sp-d16 -Og -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections  -g -I"../Library/CMSIS/Include" -I"../Library/Device/Nuvoton/M480/Include" -I"../Library/StdDriver/inc" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
